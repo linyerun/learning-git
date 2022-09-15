3cce55a8d64e71438eaef3e99d425c6b0fe77c49
Ernie <2338244917@qq.com>
2022-09-15 23:59:25 +0800

    master commit

--- src/com/lin/Main.java
+++ src/com/lin/Main.java
@@ -3,7 +3,8 @@ package com.lin;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("2");
         System.out.println("3");
-        System.out.println("3");
+        System.out.println("master commit");
     }
 }
